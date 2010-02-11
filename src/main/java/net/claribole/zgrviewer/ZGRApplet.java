f2bd593b199a9b1e23674cc2acf61140dc4d5431
epietrig <epietrig@74e165f3-c697-40a6-91af-552c8936d7fe>
2010-02-11 08:56:45 +0000

    src code indentation
    git-svn-id: https://svn.code.sf.net/p/zvtm/code/zgrviewer/trunk@2889 74e165f3-c697-40a6-91af-552c8936d7fe

--- src/main/java/net/claribole/zgrviewer/ZGRApplet.java
+++ src/main/java/net/claribole/zgrviewer/ZGRApplet.java
@@ -98,170 +98,170 @@ public class ZGRApplet extends JApplet implements MouseListener, KeyListener, ZG
     }
 
     void initGUI(){
-	this.addKeyListener(this);
-	this.addMouseListener(this);
-	// get width and height of applet panel
-	try {appletWindowWidth = Integer.parseInt(getParameter(APPLET_WIDTH_PARAM));}
-	catch(NumberFormatException ex){appletWindowWidth = DEFAULT_VIEW_WIDTH;}
-	try {appletWindowHeight = Integer.parseInt(getParameter(APPLET_HEIGHT_PARAM));}
-	catch(NumberFormatException ex){appletWindowHeight = DEFAULT_VIEW_HEIGHT;}
-	// should the navigation control panel be displayed or not
-	boolean showNavControl = true;
-	try {
-	    String s = getParameter(SHOW_NAVIGATION_CONTROLS_PARAM);
-	    if (s != null){
-		showNavControl = (new Boolean(s)).booleanValue();
-	    }
-	}
-	catch(Exception ex){showNavControl = true;}
-	boolean showFCPalette = true;
-	try {
-	    String s = getParameter(SHOW_FC_PALETTE_PARAM);
-	    if (s != null){
-            showFCPalette = (new Boolean(s)).booleanValue();
-	    }
-	}
-	catch(Exception ex){showFCPalette = true;}
-	boolean showOverview = true;
-	try {
-	    String s = getParameter(DISPLAY_OVERVIEW_PARAM);
-	    if (s != null){
-			showOverview = (new Boolean(s)).booleanValue();
-	    }
-	}
-	catch(Exception ex){showOverview = true;}
-	try {
-	    APPLET_TITLE = getParameter(APPLET_TITLE_PARAM);
-	}
-	catch(Exception ex){APPLET_TITLE = "ZGRViewer - Applet";}
-	Color APPLET_BKG_COLOR = null;
-	try {
-	    APPLET_BKG_COLOR = SVGReader.getColor(getParameter(APPLET_BKG_COLOR_PARAM));
-	}
-	catch(Exception ex){}
-	Color CURSOR_COLOR = null;
-	try {
-	    CURSOR_COLOR = SVGReader.getColor(getParameter(CURSOR_COLOR_PARAM));
-	}
-	catch(Exception ex){}
-	boolean graphBkgColorSpecified = false;
-	try {
-	    cfgMngr.backgroundColor = SVGReader.getColor(getParameter(GRAPH_BKG_COLOR_PARAM));
-	    graphBkgColorSpecified = true;
-	}
-	catch(Exception ex){
-	    cfgMngr.backgroundColor = Color.WHITE;
-	    graphBkgColorSpecified = false;
-	}
-	final boolean graphBkgColorSpecifiedF = graphBkgColorSpecified;
-	String centerOnLabel = null;
-	try {
-	    centerOnLabel = getParameter(CENTER_ON_LABEL_PARAM);
-	}
-	catch (Exception ex){
-	    centerOnLabel = null;
-	}
-	final String centerOnLabelF = centerOnLabel;
-	boolean antialiased = true;
-	try {
-	    String s = getParameter(ANTIALIASING_PARAM);
-	    if (s != null){
-		antialiased = (new Boolean(s)).booleanValue();
-	    }
-	}
-	catch(Exception ex){antialiased = true;}
-	try {
-	    String s = getParameter(HIGHLIGHT_COLOR_PARAM);
-	    if (s != null){
-		ConfigManager.HIGHLIGHT_COLOR = SVGReader.getColor(s);
-	    }
-	}
-	catch(Exception ex){ConfigManager.HIGHLIGHT_COLOR = Color.RED;}
-	try {
-	    String s = getParameter(FOCUS_NODE_MAG_FACTOR_PARAM);
-	    if (s != null){
-		    ConfigManager.MAG_FACTOR = Float.parseFloat(s);
-	    }
-	}
-	catch(Exception ex){ConfigManager.MAG_FACTOR = 2.0f;}
-	AppletUtils.initLookAndFeel();
-	Container cpane = getContentPane();
-	this.setSize(appletWindowWidth-10, appletWindowHeight-10);
-	cpane.setSize(appletWindowWidth, appletWindowHeight);
-	cpane.setBackground(APPLET_BKG_COLOR);
-	viewPanel = grMngr.createPanelView(grMngr.createZVTMelements(true), appletWindowWidth, appletWindowHeight-40);
-	meh = new ZgrAppletEvtHdlr(this, this.grMngr);
-	grMngr.parameterizeView(meh);
- 	viewPanel.setPreferredSize(new Dimension(appletWindowWidth-10, appletWindowHeight-40));
-	grMngr.mainView.setAntialiasing(antialiased);
-	statusBar = new JLabel(Messages.EMPTY_STRING);
-	JPanel borderPanel = new JPanel();
-	borderPanel.setLayout(new BorderLayout());
-	borderPanel.add(viewPanel, BorderLayout.CENTER);
-	borderPanel.add(statusBar, BorderLayout.SOUTH);
-	borderPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1), APPLET_TITLE));
-	borderPanel.setOpaque(false);
-	if (showNavControl || showOverview){
-	    GridBagLayout gridBag = new GridBagLayout();
-	    GridBagConstraints constraints = new GridBagConstraints();
-	    constraints.fill = GridBagConstraints.BOTH;
-	    constraints.anchor = GridBagConstraints.CENTER;
-	    cpane.setLayout(gridBag);
-	    buildConstraints(constraints,0,0,1,1,90,100);
-	    gridBag.setConstraints(borderPanel, constraints);
-	    cpane.add(borderPanel);
-	    navPanel = new NavPanel(grMngr, centerOnLabelF, showOverview);
-	    buildConstraints(constraints,1,0,1,1,10,0);
-	    gridBag.setConstraints(navPanel, constraints);
-	    cpane.add(navPanel);
-	}
-	else {
-	    cpane.add(borderPanel);
-	}
-	if (CURSOR_COLOR != null){
-	    grMngr.mainView.mouse.setColor(CURSOR_COLOR);
-	    grMngr.mainView.mouse.setHintColor(CURSOR_COLOR);
-	}
-    grMngr.tp.setEnabled(showFCPalette);
-    gp = new ZGRAGlassPane(appletWindowWidth, appletWindowHeight);
-    this.setGlassPane(gp);
-    gp.setVisible(true);
-    gp.setMessage(Messages.LOADING_SVG);
-	final SwingWorker worker = new SwingWorker(){
-		public Object construct(){
-		    sleep(1000);
-		    requestFocus();
-		    grMngr.vsm.repaintNow();
-            URL svgUrl = null;
-            try {
-                svgUrl = new URL(getDocumentBase(), getParameter(SVG_FILE_URL_PARAM));
+        this.addKeyListener(this);
+        this.addMouseListener(this);
+        // get width and height of applet panel
+        try {appletWindowWidth = Integer.parseInt(getParameter(APPLET_WIDTH_PARAM));}
+        catch(NumberFormatException ex){appletWindowWidth = DEFAULT_VIEW_WIDTH;}
+        try {appletWindowHeight = Integer.parseInt(getParameter(APPLET_HEIGHT_PARAM));}
+        catch(NumberFormatException ex){appletWindowHeight = DEFAULT_VIEW_HEIGHT;}
+        // should the navigation control panel be displayed or not
+        boolean showNavControl = true;
+        try {
+            String s = getParameter(SHOW_NAVIGATION_CONTROLS_PARAM);
+            if (s != null){
+                showNavControl = (new Boolean(s)).booleanValue();
             }
-            catch (MalformedURLException e) {
-                //TODO
-                e.printStackTrace();
-            }
-            gvLdr.loadSVG(svgUrl.toString());
-		    // override SVG's background color if background color is specified in applet params
-		    if (graphBkgColorSpecifiedF){grMngr.mainView.setBackgroundColor(cfgMngr.backgroundColor);}
-		    grMngr.tp.updateHiddenPosition();
-		    grMngr.vsm.repaintNow(grMngr.mainView, ZGRApplet.this);
-		    while (!ZGRApplet.this.paintedAtLeastOnce){
-			sleep(500);
-		    }
-		    if (centerOnLabelF != null){
-			grMngr.search(centerOnLabelF, 1);
-		    }
-		    grMngr.vsm.repaintNow();
-		    gp.setVisible(false);
-		    return null; 
-		}
-
-        public void finished(){
-            setStatusBarText(Messages.SPACE_STRING);
         }
+        catch(Exception ex){showNavControl = true;}
+        boolean showFCPalette = true;
+        try {
+            String s = getParameter(SHOW_FC_PALETTE_PARAM);
+            if (s != null){
+                showFCPalette = (new Boolean(s)).booleanValue();
+            }
+        }
+        catch(Exception ex){showFCPalette = true;}
+        boolean showOverview = true;
+        try {
+            String s = getParameter(DISPLAY_OVERVIEW_PARAM);
+            if (s != null){
+                showOverview = (new Boolean(s)).booleanValue();
+            }
+        }
+        catch(Exception ex){showOverview = true;}
+        try {
+            APPLET_TITLE = getParameter(APPLET_TITLE_PARAM);
+        }
+        catch(Exception ex){APPLET_TITLE = "ZGRViewer - Applet";}
+        Color APPLET_BKG_COLOR = null;
+        try {
+            APPLET_BKG_COLOR = SVGReader.getColor(getParameter(APPLET_BKG_COLOR_PARAM));
+        }
+        catch(Exception ex){}
+        Color CURSOR_COLOR = null;
+        try {
+            CURSOR_COLOR = SVGReader.getColor(getParameter(CURSOR_COLOR_PARAM));
+        }
+        catch(Exception ex){}
+        boolean graphBkgColorSpecified = false;
+        try {
+            cfgMngr.backgroundColor = SVGReader.getColor(getParameter(GRAPH_BKG_COLOR_PARAM));
+            graphBkgColorSpecified = true;
+        }
+        catch(Exception ex){
+            cfgMngr.backgroundColor = Color.WHITE;
+            graphBkgColorSpecified = false;
+        }
+        final boolean graphBkgColorSpecifiedF = graphBkgColorSpecified;
+        String centerOnLabel = null;
+        try {
+            centerOnLabel = getParameter(CENTER_ON_LABEL_PARAM);
+        }
+        catch (Exception ex){
+            centerOnLabel = null;
+        }
+        final String centerOnLabelF = centerOnLabel;
+        boolean antialiased = true;
+        try {
+            String s = getParameter(ANTIALIASING_PARAM);
+            if (s != null){
+                antialiased = (new Boolean(s)).booleanValue();
+            }
+        }
+        catch(Exception ex){antialiased = true;}
+        try {
+            String s = getParameter(HIGHLIGHT_COLOR_PARAM);
+            if (s != null){
+                ConfigManager.HIGHLIGHT_COLOR = SVGReader.getColor(s);
+            }
+        }
+        catch(Exception ex){ConfigManager.HIGHLIGHT_COLOR = Color.RED;}
+        try {
+            String s = getParameter(FOCUS_NODE_MAG_FACTOR_PARAM);
+            if (s != null){
+                ConfigManager.MAG_FACTOR = Float.parseFloat(s);
+            }
+        }
+        catch(Exception ex){ConfigManager.MAG_FACTOR = 2.0f;}
+        AppletUtils.initLookAndFeel();
+        Container cpane = getContentPane();
+        this.setSize(appletWindowWidth-10, appletWindowHeight-10);
+        cpane.setSize(appletWindowWidth, appletWindowHeight);
+        cpane.setBackground(APPLET_BKG_COLOR);
+        viewPanel = grMngr.createPanelView(grMngr.createZVTMelements(true), appletWindowWidth, appletWindowHeight-40);
+        meh = new ZgrAppletEvtHdlr(this, this.grMngr);
+        grMngr.parameterizeView(meh);
+        viewPanel.setPreferredSize(new Dimension(appletWindowWidth-10, appletWindowHeight-40));
+        grMngr.mainView.setAntialiasing(antialiased);
+        statusBar = new JLabel(Messages.EMPTY_STRING);
+        JPanel borderPanel = new JPanel();
+        borderPanel.setLayout(new BorderLayout());
+        borderPanel.add(viewPanel, BorderLayout.CENTER);
+        borderPanel.add(statusBar, BorderLayout.SOUTH);
+        borderPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1), APPLET_TITLE));
+        borderPanel.setOpaque(false);
+        if (showNavControl || showOverview){
+            GridBagLayout gridBag = new GridBagLayout();
+            GridBagConstraints constraints = new GridBagConstraints();
+            constraints.fill = GridBagConstraints.BOTH;
+            constraints.anchor = GridBagConstraints.CENTER;
+            cpane.setLayout(gridBag);
+            buildConstraints(constraints,0,0,1,1,90,100);
+            gridBag.setConstraints(borderPanel, constraints);
+            cpane.add(borderPanel);
+            navPanel = new NavPanel(grMngr, centerOnLabelF, showOverview);
+            buildConstraints(constraints,1,0,1,1,10,0);
+            gridBag.setConstraints(navPanel, constraints);
+            cpane.add(navPanel);
+        }
+        else {
+            cpane.add(borderPanel);
+        }
+        if (CURSOR_COLOR != null){
+            grMngr.mainView.mouse.setColor(CURSOR_COLOR);
+            grMngr.mainView.mouse.setHintColor(CURSOR_COLOR);
+        }
+        grMngr.tp.setEnabled(showFCPalette);
+        gp = new ZGRAGlassPane(appletWindowWidth, appletWindowHeight);
+        this.setGlassPane(gp);
+        gp.setVisible(true);
+        gp.setMessage(Messages.LOADING_SVG);
+        final SwingWorker worker = new SwingWorker(){
+            public Object construct(){
+                sleep(1000);
+                requestFocus();
+                grMngr.vsm.repaintNow();
+                URL svgUrl = null;
+                try {
+                    svgUrl = new URL(getDocumentBase(), getParameter(SVG_FILE_URL_PARAM));
+                }
+                catch (MalformedURLException e) {
+                    //TODO
+                    e.printStackTrace();
+                }
+                gvLdr.loadSVG(svgUrl.toString());
+                // override SVG's background color if background color is specified in applet params
+                if (graphBkgColorSpecifiedF){grMngr.mainView.setBackgroundColor(cfgMngr.backgroundColor);}
+                grMngr.tp.updateHiddenPosition();
+                grMngr.vsm.repaintNow(grMngr.mainView, ZGRApplet.this);
+                while (!ZGRApplet.this.paintedAtLeastOnce){
+                    sleep(500);
+                }
+                if (centerOnLabelF != null){
+                    grMngr.search(centerOnLabelF, 1);
+                }
+                grMngr.vsm.repaintNow();
+                gp.setVisible(false);
+                return null; 
+            }
 
-	    };
-	worker.start();
+            public void finished(){
+                setStatusBarText(Messages.SPACE_STRING);
+            }
+
+        };
+        worker.start();
     }
 
     boolean paintedAtLeastOnce = false;
