e4ebe0832f25a162cc820402f32363e2e53f7ee4
epietrig <epietrig@74e165f3-c697-40a6-91af-552c8936d7fe>
2015-02-06 11:54:34 +0000

    started working on smarties control for wall version
    git-svn-id: https://svn.code.sf.net/p/zvtm/code/zgrviewer/trunk@5328 74e165f3-c697-40a6-91af-552c8936d7fe

--- src/main/java/net/claribole/zgrviewer/WallZGRViewer.java
+++ src/main/java/net/claribole/zgrviewer/WallZGRViewer.java
@@ -15,12 +15,24 @@ import fr.inria.zvtm.engine.VirtualSpaceManager;
 import fr.inria.zvtm.cluster.ClusterGeometry;
 import fr.inria.zvtm.cluster.ClusteredView;
 
+import fr.lri.smarties.libserver.Smarties;
+import fr.lri.smarties.libserver.SmartiesColors;
+import fr.lri.smarties.libserver.SmartiesEvent;
+import fr.lri.smarties.libserver.SmartiesPuck;
+import fr.lri.smarties.libserver.SmartiesDevice;
+import fr.lri.smarties.libserver.SmartiesWidget;
+import fr.lri.smarties.libserver.SmartiesWidgetHandler;
+import java.util.Observer;
+import java.util.Observable;
+
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
 public class WallZGRViewer extends ZGRViewer {
 
     ClusterGeometry cg;
+
+    SmartiesManager stm;
 
     public WallZGRViewer(ZGROptions options){
         super(options);
@@ -44,6 +56,24 @@ public class WallZGRViewer extends ZGRViewer {
         ClusteredView cv = new ClusteredView(cg, options.numRows-1, options.numCols, options.numRows, ccameras);
         VirtualSpaceManager.INSTANCE.addClusteredView(cv);
         cv.setBackgroundColor(cfgMngr.backgroundColor);
+        stm = new SmartiesManager(this);
+    }
+
+    // for Smarties config
+    int getDisplayWidth(){
+        return cg.getWidth();
+    }
+
+    int getDisplayHeight(){
+        return cg.getHeight();
+    }
+
+    int getColumnCount(){
+        return cg.getColumns();
+    }
+
+    int getRowCount(){
+        return cg.getRows();
     }
 
     public static void main(String[] args){
@@ -62,5 +92,92 @@ public class WallZGRViewer extends ZGRViewer {
         System.out.println("--help for command line options");
         new WallZGRViewer(options);
     }
+
+}
+
+class SmartiesManager implements Observer {
+
+    WallZGRViewer application;
+
+    Smarties smarties;
+
+    SmartiesPuck pinchPuck;
+    SmartiesDevice pinchDevice;
+    SmartiesPuck dragPuck;
+    SmartiesDevice dragDevice;
+
+    float prevMFPinchD = 0;
+    float prevMFMoveX = 0;
+    float prevMFMoveY = 0;
+
+    SmartiesManager(WallZGRViewer app){
+        this.application = app;
+
+        smarties = new Smarties(application.getDisplayWidth(), application.getDisplayHeight(),
+                                application.getColumnCount(), application.getRowCount());
+
+        smarties.initWidgets(3,2);
+
+        // SmartiesWidget sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_BUTTON,
+        //                                        "<", 1,1,1,1);
+        // sw.handler = new PrevTimeStepEvent();
+
+        // sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_BUTTON,
+        //                                        ">", 2,1,1,1);
+        // sw.handler = new NextTimeStepEvent();
+
+        // sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_BUTTON,
+        //                                        "Home", 3,1,1,1);
+        // sw.handler = new HomeEvent();
+
+        // sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_TOGGLE_BUTTON,
+        //                                        "SHP", 1,2,1,1);
+        // sw.on = true;
+        // sw.handler = new ToggleShpEvent();
+
+        // sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_TOGGLE_BUTTON,
+        //                                        "VEG", 2,2,1,1);
+        // sw.on = true;
+        // sw.handler = new ToggleVTEvent();
+
+        // sw = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_TOGGLE_BUTTON,
+        //                                        "SITES", 3,2,1,1);
+        // sw.on = true;
+        // sw.handler = new ToggleSitesEvent();
+
+        smarties.addObserver(this);
+
+        smarties.Run();
+    }
+
+
+    public void update(Observable obj, Object arg){
+        // if (arg instanceof SmartiesEvent) {
+        //     final SmartiesEvent se = (SmartiesEvent)arg;
+        //     switch (se.type){
+        //         case SmartiesEvent.SMARTIE_EVENTS_TYPE_WIDGET:{
+        //             if (se.widget.handler != null) se.widget.handler.callback(se.widget, se, this);
+        //             break;
+        //         }
+        //         default:{break;}
+        //     }
+        // } else {
+        //     System.out.println("!(arg instanceof SmartiesEvent)");
+        // }
+    }
+
+    // class HomeEvent implements SmartiesWidgetHandler {
+    //     public boolean callback(SmartiesWidget sw, SmartiesEvent se, Object user_data){
+    //         application.mCamera.setLocation(application.gm.getKlabinCenterCoords());
+    //         return true;
+    //     }
+    // }
+
+    // class ToggleShpEvent implements SmartiesWidgetHandler {
+    //     public boolean callback(SmartiesWidget sw, SmartiesEvent se, Object user_data){
+    //         application.gm.toggleContourShape();
+    //         return true;
+    //     }
+    // }
 
 }
